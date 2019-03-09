1e2c90406b400343cb1e0669eea405e9b1bea354
suntongf@126.com <suntongf@126.com>
2019-03-09 17:01:46 +0800

    完成初始效果

---
+++ app/src/main/java/com/kk/tongfu/douyinloading/view/LoadingView.java
@@ -0,0 +1,144 @@
+package com.kk.tongfu.douyinloading.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+/**
+ * Created by tongfu
+ * on 2019/3/9
+ * Desc:
+ */
+
+public class LoadingView extends View {
+
+
+    private int mWidth,mHeight;
+    private int mDefaultWidth,mDefaultHeight;
+    private int mProgressWidth;
+    private Paint mPaint;
+    private String mColor;
+
+    public LoadingView(Context context) {
+        this(context,null);
+    }
+
+    public LoadingView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs,0);
+    }
+
+    public LoadingView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        //设置view的默认最小宽度
+        mDefaultWidth=600;
+        //设置view的默认最小高度
+        mDefaultHeight=5;
+        //设置进度条的初始宽度,这个宽度不能大于view的最小宽度，否则进度条不能向两边延伸
+        mProgressWidth=100;
+        //设置默认初始颜色
+        mColor="#808080";
+
+        mPaint=new Paint();
+        //设置虎逼模式为填充带边框
+        mPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+        //设置抗锯齿
+        mPaint.setAntiAlias(true);
+
+
+    }
+
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        //通过widthMeasureSpec,heightMeasureSpec 来获取view的测量模式和宽高
+        int width = getValue(widthMeasureSpec,true);
+        int height = getValue(heightMeasureSpec,false);
+
+        //此方法用来设置设置View的具体宽高
+        setMeasuredDimension(width, height);
+    }
+
+    /**
+     * 获取view的宽高值
+     * @param measureSpec
+     * @param isWidth 是否是测量宽度
+     * @return
+     */
+    private int getValue(int measureSpec,boolean isWidth) {
+        //获取测量模式
+        int mode = MeasureSpec.getMode(measureSpec);
+        //获取测量的值
+        int size = MeasureSpec.getSize(measureSpec);
+        switch (mode) {
+            /**
+             * 如果父控件传递给的MeasureSpec的mode是MeasureSpec.UNSPECIFIED，就说明，父控件对自己没有任何限制，那么尺寸就选择自己需要的尺寸size
+
+             如果父控件传递给的MeasureSpec的mode是MeasureSpec.EXACTLY，就说明父控件有明确的要求，希望自己能用measureSpec中的尺寸，这时就推荐使用MeasureSpec.getSize(measureSpec)
+
+             如果父控件传递给的MeasureSpec的mode是MeasureSpec.AT_MOST，就说明父控件希望自己不要超出MeasureSpec.getSize(measureSpec)，如果超出了，就选择MeasureSpec.getSize(measureSpec)，否则用自己想要的尺寸就行了
+             */
+            case MeasureSpec.EXACTLY:
+                 //子view的大小已经被限定死，我们只能使用其固定大小
+                return size;
+            case MeasureSpec.AT_MOST:
+                //父控件认为子view的大小不能超过size的值，那么我们就取size和默认值之间的最小值
+                return Math.min(isWidth?mDefaultWidth:mDefaultHeight, size);
+            case MeasureSpec.UNSPECIFIED:
+                //父view不限定子view的大小，我们将其值设置为默认值
+                return isWidth?mDefaultWidth:mDefaultHeight;
+            default:
+                return isWidth?mDefaultWidth:mDefaultHeight;
+        }
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mWidth=w;
+        mHeight=h;
+        mPaint.setStrokeWidth(mHeight);
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        //首先判断进度条的宽度是否大于view宽度
+        if(mProgressWidth<mWidth){
+            //如果不大于，将进度条宽度增加10
+            mProgressWidth+=10;//注意执行此步骤是mProgressWidth值有可能大于view宽度；
+        }else{
+            //如果进度条宽度大于view宽度将进度条宽度设置为初始宽度
+            mProgressWidth=100;
+        }
+        //计算颜色透明度
+        //mProgressWidth/mWidth 计算当前进度条宽度占总宽度的比例
+        //255*mProgressWidth/mWidth 计算当前比例下对应的透明度值
+        //由于是由不透明变成全透明，所以使用255减去其值
+        int currentColorValue=255-255*mProgressWidth/mWidth;
+
+        if(currentColorValue>255){
+            //由于mProgressWidth有可能大于view的宽度，要保证颜色值不能大于255
+            currentColorValue=255;
+        }
+        if(currentColorValue<30){
+            //此处是为了限制让其不成为全透明，如果设置为全透明，在最后阶段进度宽度渐变观察不到
+            currentColorValue=30;
+        }
+        //将透明度转换为16进制
+        String s = Integer.toHexString(currentColorValue);
+        //拼接颜色字符串并转化为颜色值
+        int color = Color.parseColor("#" + s + mColor.substring(1,mColor.length()));
+        //给画笔设置颜色
+        mPaint.setColor(color);
+        //使用canvas来画进度条（确实就是画一条线）
+        canvas.drawLine(mWidth/2-mProgressWidth/2,mDefaultHeight/2,mWidth/2+mProgressWidth/2,mDefaultHeight/2,mPaint);
+        invalidate();
+    }
+}
--- introduce.md
+++ introduce.md
@@ -10,11 +10,59 @@
 ### 效果分析
 首先我们来分析这个进度条的效果，可以看到刚开始进度条是固定长度的深灰色的，然后慢慢向两端延伸并且颜色渐变成透明色，到达屏幕两边时完全透明，如此循环，在这个过程中有两个要点：
 
->* 1 颜色渐变，深灰色>透明色
-* 2 长度向外延伸，由中间向两侧延伸，至屏幕边界
+>* 1 长度向外延伸，由中间向两侧延伸，至屏幕边界
+* 2 颜色渐变，深灰色>透明色
 
-首先我们来分析颜色渐变，项目中我们常用的颜色表示方式有两种，一种是不带透明度，一种是带透明度，如下所示
+首先我们来分析长度向外延伸至屏幕边界，首先我们可以获取到整个view的高度，我们可以在view中心位置上画一个固定初始宽度和高度的灰色的进度条（其实就是一条灰色的线），然后每次像两边增加固定的宽度，然后再次重新绘制这个view，直到进度条的宽度大于view的宽度时，我们再次将进度条的宽度设置为初始宽度。
+
+
+接着我们来分析颜色渐变，项目中我们常用的颜色表示方式有两种，一种是不带透明度，一种是带透明度，如下所示
+
 <img src="https://raw.githubusercontent.com/tongfuzz/douyinloading/master/app/screenshots/color.png" />
+
+如果我们直接改变颜色的值，由于颜色值不易控制，改变颜色值的过程中很容易出现我们不需要的颜色，所以我们可以换一种思路，通过改变颜色的透明度来达到我们的效果，我们将最开始的深灰色，通过不断加大它的透明度，最后使其变为透明色不可见，颜色的透明度由两位16进制数来表示，为ff时表明不透明，为00时表示全透明，如下图
+
+<img src="https://raw.githubusercontent.com/tongfuzz/douyinloading/master/app/screenshots/color2.png" />
+
+我们知道两位16进制数表示的范围0-255，在进度条为初始宽度时设置透明度为255，随着进度条宽度逐渐增大，我们将透明度由255逐渐减小，最后变为接近全透明0（为何不是全透明后面会解释），好了，废话不多说，开始上代码。
+
+---
+
+首先我们创建一个类LoadingView继承View类，并重写它的构造方法，最后一个构造方法我们一般用不到，删除就行了，让前两个构造方法调用第三个构造方法，以保证每次都会调用到第三个构造方法，如下图
+
+<img src="https://raw.githubusercontent.com/tongfuzz/douyinloading/master/app/screenshots/view2.png" />
+
+接下来我们给view设置一个默认最小宽度和高度，以及进度条的初始宽度，初始颜色 并创建画笔
+
+<img src="https://raw.githubusercontent.com/tongfuzz/douyinloading/master/app/screenshots/view3.png" />
+
+然后就是自定义view三部曲了，首先我们重写onMeasure方法来测量view的宽高，具体实现如下图，（每一步的说明已经写的很详细不再叙述，如果对MeasureSpec不是很了解的同学可以参考此文[MeasureSpec详解](https://segmentfault.com/a/1190000007948959)）
+
+<img src="https://raw.githubusercontent.com/tongfuzz/douyinloading/master/app/screenshots/view4.png" />
+
+onSizeChanged方法会在view的大小发生改变时调用，所以获取完view的宽高后我们重写onSizeChanged方法将宽高赋值给全局变量，并设置画笔高度，如下图
+
+<img src="https://raw.githubusercontent.com/tongfuzz/douyinloading/master/app/screenshots/view5.png" />
+
+接着我们重写onDraw方法开始view的绘制，具体过程如下，首先保证进度条宽度不能大于view的宽度，然后根据进度条宽度占比来计算颜色的透明度，最后给画笔设置为此颜色，然后用canvas来画一条从view的中心点开始向左右画一条宽度为进度条二分之一的线，(关于canvas的使用方法不了解的同学可以参考此文[Canvas使用详解](http://www.gcssloop.com/customview/Canvas_BasicGraphics)) 如下图
+
+<img src="https://raw.githubusercontent.com/tongfuzz/douyinloading/master/app/screenshots/view6.png" />
+
+最后调用 invalidate()方法来不断刷新重新绘制，由于在重新绘制的过程中进度条宽度不断改变，这样就形成了进度条不断延伸的效果，效果如下
+
+ <div align=center>
+ <img width="300" height="500" src="https://raw.githubusercontent.com/tongfuzz/douyinloading/master/app/screenshots/hehe.gif"/>
+ </div>
+
+
+
+
+
+
+
+
+
+
 
 
 
